83bb92bb070e50f6cdb214fbc079acc83494f5d8
Giovanni Daldegan <giovannidaldegani@gmail.com>
2025-10-14 12:27:45 -0300

    dados facebook

---
+++ main.py
@@ -0,0 +1,8 @@
+import networkx
+
+
+# importar 2000 nós aleatórios do grafo com NetworkX
+# mapear comunidades - algoritmo de Louvain
+# calcular medidas de centralidade
+# criar visualização gráfica das medidas - nós com rótulos e valores, mapas em cores das comunidades
+# elaborar relatório de análise do grafo sobre os nós mais significativos
